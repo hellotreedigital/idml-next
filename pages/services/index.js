af8fdfa09fc0b49384ddb1518c9a3b5fc73886e2
nicolassaikali <hellotreedigital>
2022-04-14 16:29:41 +0300

    errors

--- pages/services/index.js
+++ pages/services/index.js
@@ -28,6 +28,7 @@ export default function Services(props) {
 
     return (
         <Layout activePage="services" menuItems={menuItems} socialMedia={socialMedia} footerLogos={footerLogos} footerContactIcons={footerContactIcons} serviceTitles={serviceTitles}  industriesTitles={industriesTitles}>
+          
             {
                 serviceSetting ?
                     <Banner
