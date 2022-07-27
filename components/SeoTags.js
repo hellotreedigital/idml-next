21b6c049c05797451e957754d1bdf1a07c8ebc69
nicolassaikali <hellotreedigital>
2022-07-27 13:56:18 +0300

    fixed error

--- components/SeoTags.js
+++ components/SeoTags.js
@@ -2,24 +2,23 @@ import Head from 'next/head';
 
 export default function SeoTags(props) {
     return (
-        <>
-            <Head>
-                {/* Primary Meta Tags */}
-                <title key={props.title}>{props.title}</title>
-                <meta name="title" content={props.title} key={props.title} />
-                <meta name="description" content={props.description} />
-                {/* <!-- Open Graph / Facebook --> */}
-                <meta property="og:type" content="website" />
-                <meta property="og:title" content={props.title} key={props.title} />
-                <meta property="og:description" content={props.description} />
-                <meta property="og:image" content={props.image} />
-                {/* <!-- Twitter --> */}
-                <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:title" content={props.title} key={props.title} />
-                <meta property="twitter:description" content={props.description} />
-                <meta property="twitter:image" content={props.image} />
+        <Head>
+            {/* Primary Meta Tags */}
+            <title>{props.title}</title>
+            <meta name="title" content={props.title} />
+            <meta name="description" content={props.description} />
+            {/* <!-- Open Graph / Facebook --> */}
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={props.title} />
+            <meta property="og:description" content={props.description} />
+            <meta property="og:image" content={props.image} />
+            {/* <!-- Twitter --> */}
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:title" content={props.title} />
+            <meta property="twitter:description" content={props.description} />
+            <meta property="twitter:image" content={props.image} />
 
-            </Head>
+
 
             {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
             <script
@@ -37,6 +36,6 @@ export default function SeoTags(props) {
 
             <meta name="google-site-verification" content="fk70t976Le28xv3Fe9p6qFejKO_mv7LcVTvhuSO0LFU" />
 
-        </>
+        </Head>
     )
 }
